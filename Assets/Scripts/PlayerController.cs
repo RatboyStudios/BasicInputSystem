d9ef717dbc72e17161dbca0af71c205637991e13
Danny Govantes <32424801+DannyGovantes@users.noreply.github.com>
2020-07-03 15:31:59 -0500

    First commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+namespace RatboyStudios.PlayerInput
+{
+    [RequireComponent(typeof(PlayerInput))]
+    public class PlayerController : MonoBehaviour
+    {
+
+        private PlayerInput _playerInput;
+        private Animator _animator;
+       
+
+        private readonly int HashCrouch = Animator.StringToHash("Crouch");
+        private readonly int HashAttack = Animator.StringToHash("Attack");
+        private readonly int HashVerticalSpeed = Animator.StringToHash("VerticalSpeed");
+        private readonly int HashHorizontalSpeed = Animator.StringToHash("HorizontalSpeed");
+
+
+        private void Start()
+        {
+            if (!TryGetComponent(out _animator))
+            {
+                Debug.LogError("MISSING ANIMATOR",this);
+            }
+            if (!TryGetComponent(out _playerInput))
+            {
+                Debug.LogError("MISSING PLAYERINPUT",this);
+            } 
+         
+            
+        }
+
+        private void Update()
+        {
+            if (_playerInput.Pause)
+            {
+                Time.timeScale = 0.0f;
+            }
+        }
+
+        private void HorizontalMovement()
+        {
+            _animator.SetFloat(HashHorizontalSpeed,_playerInput.Horizontal);
+         
+
+        }
+
+        private void VerticalMovement()
+        {
+            _animator.SetFloat(HashVerticalSpeed,_playerInput.Vertical);
+        }
+
+        private void Crouch()
+        {
+            _animator.SetBool(HashCrouch,_playerInput.Crouch);
+        }
+        
+
+        private void FixedUpdate()
+        {
+            _animator.ResetTrigger(HashAttack);
+
+            if (_playerInput.Attack)
+            {
+                _animator.SetTrigger(HashAttack);
+            }
+            HorizontalMovement();
+            VerticalMovement();
+            Crouch();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+namespace RatboyStudios.PlayerInput
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        
+        //DETECTAR LA TECLA IZQUIERDA
+        //DETECTAR LA TECLA DERECHA
+        [SerializeField] private float _horizontal;
+        public float Horizontal
+        {
+            get { return _horizontal; }
+        }
+
+        //DETECTAR LA TECLA ARRIBA
+        //DETECTAR LA TECLA ABAJO
+        [SerializeField] private float _vertical;
+        public float Vertical
+        {
+            get { return _vertical; }
+        }
+
+        //DETECTAR LA TECLA DE P PARA PAUSAR
+        [SerializeField] private bool _pause;
+        public bool Pause
+        {
+            get { return _pause; }
+        }
+
+        //DETECTAR LA TECLA DE E PARA ATACAR
+
+        [SerializeField] private bool _attack;
+        
+        public bool Attack
+        {
+            get { return _attack; }
+        }
+        
+        //DETECTAR LA TECLA C PARA AGACHARSE
+
+        [SerializeField] private bool _crouch;
+        
+        public bool Crouch
+        {
+            get { return _crouch; }
+        }
+        //CHECAR SI ESTÁ ACTIVADO O DESACTIVADO MI INPUT
+
+        [SerializeField] private bool _inputEnabled = false;
+        
+        public bool InputEnabled
+        {
+            get
+            {
+                return _inputEnabled; 
+                
+            }
+            set { _inputEnabled = value; }
+        }
+
+
+        private void Update()
+        {
+            if (_inputEnabled)
+            {
+                _horizontal = Input.GetAxisRaw("Horizontal");
+                _vertical = Input.GetAxisRaw("Vertical");
+                _pause = Input.GetKeyDown(KeyCode.P);
+                _attack = Input.GetKeyUp(KeyCode.E);
+                _crouch = Input.GetKey(KeyCode.C);
+            }
+            else
+            {
+                _horizontal = 0f;
+                _vertical = 0f;
+                _pause = false;
+                _attack = false;
+                _crouch = false;
+            }
+        }
+    }
+}
